98cddef4c946954744bc75ed1809b2be261b6e2f
Ray King <taipan.king@gmail.com>
2017-12-13 13:56:25 +1100

    Updated: cli project add

--- bin/criticide-project-add.js
+++ bin/criticide-project-add.js
@@ -5,53 +5,60 @@ const ask = require("inquirer");
 const R = require("ramda");
 const S = require("string");
 
-const project = require("../lib/project.js");
-
-const readInput = cliInput =>
-  R.dissoc(
-    "args",
-    R.assoc(
-      "name",
-      R.head(cliInput.args),
-      R.pickAll(["args", "force", "default"], cliInput)
-    )
-  );
+const input = require("./elements/readInput");
 
 const askUser = async payload => {
-  return await ask.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What name would you like to give this project",
-      validate: value =>
-        R.isEmpty(value) ? "A project requires a name" : true,
-      filter: value => S(value).titleCase().s
-    },
-    {
-      type: "confirm",
-      name: "force",
-      message: "Overwrite exist project with the same name",
-      default: R.not(R.isNil(payload.force))
-    },
-    {
-      type: "confirm",
-      name: "default",
-      message: "Set this new project as the default project",
-      default: R.not(R.isNil(payload.default))
-    }
-  ]);
+  return await ask
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What name would you like to give this project",
+        validate: value =>
+          R.isEmpty(value) ? "A project requires a name" : true,
+        filter: value => S(value).titleCase().s,
+        when: R.not(payload.args)
+      },
+      {
+        type: "input",
+        name: "label",
+        message: "Label to attach this project",
+        default: payload.label,
+        when: R.and(payload.label, R.not(payload.args))
+      },
+      {
+        type: "confirm",
+        name: "force",
+        message: "Overwrite exist project with the same name",
+        default: payload.force,
+        when: R.and(payload.force, R.not(payload.args))
+      },
+      {
+        type: "confirm",
+        name: "default",
+        message: "Set this new project as the default project",
+        default: payload.default,
+        when: R.and(payload.default, R.not(payload.args))
+      }
+    ])
+    .then(collected => {
+      return R.merge(payload, collected);
+    });
 };
 
 addProject
   .description("Creates a new project for modeling and simulation.")
   .option(
-    "-d --default",
-    "set the newly created project as the default project."
+    "-d, --default",
+    "set the newly created project as the default project"
   )
   .option(
-    "-f --force",
-    "overwrites any existing project that has the same name, all associated project data is removed."
+    "-f, --force",
+    "overwrites any existing project that has the same name, all associated project data is removed"
   )
+  .option("-l, --label <name>", "assign label to new project")
   .parse(process.argv);
 
-askUser(addProject).then(result => console.log(result));
+askUser(input.mapFromCli(addProject)).then(answer => {
+  console.log("Payload for API => " + JSON.stringify(answer));
+});
---
+++ bin/elements/readInput.js
@@ -0,0 +1,26 @@
+const R = require("ramda");
+const S = require("string");
+
+/**
+ * Reads the command line input.
+ * @param rawInputs
+ * @returns {(Record<string, any> & any) | <T, U>(val: T, obj: U) => (Record<K, T> & U) | <U>(obj: U) => (Record<K, T> & U)}
+ */
+const mapFromCli = rawInputs => {
+  const optionKeyList = R.map(
+    R.drop(2),
+    R.map(R.prop("long"), rawInputs.options)
+  );
+  return R.assoc(
+    "args",
+    S(R.head(rawInputs.args)).titleCase().s,
+    R.zipObj(
+      optionKeyList,
+      R.map(R.propOr(false, R.__, rawInputs), optionKeyList)
+    )
+  );
+};
+
+module.exports = {
+  mapFromCli
+};
